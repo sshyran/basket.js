84e2880ad58d303bcb8b7d74a9dc85de1a8d369d
addyosmani <addyosmani@gmail.com>
2012-03-03 13:50:31 +0000

    updating to latest

--- basket.js
+++ basket.js
@@ -2,148 +2,142 @@
  * A script-loader that handles caching scripts in localStorage
  * where supported.
  * http://addyosmani.com/
- * Credits: Addy Osmani, Mathias Bynens, Ironsjp.
- * Copyright (c) 2012 Addy Osmani; 
- * Licensed MIT, GPL 
+ * Credits: Addy Osmani, Ironsjp, Mathias Bynens, Rick Waldron.
+ * Copyright (c) 2012 Addy Osmani;
+ * Licensed MIT, GPL
  */
- ;(function (w, d) {
+ ;(function (window, document) {
 
-    function basketLoader() {
+	function basketLoader() {
 
-        var
-        _storagePrefix = "basket-",
-            _localStorage = function (a, b) {
-                try {
-                    return (a = localStorage).setItem(b, a), a.removeItem(b), !0
-                } catch (c) {}
-            }(),
-            scripts = [],
-            scriptsExecuted = 0,
-            waitCount = 0,
-            waitCallbacks = [],
+		var
+		_storagePrefix = "basket-",
+			scripts = [],
+			scriptsExecuted = 0,
+			waitCount = 0,
+			waitCallbacks = [],
 
-            // Minimalist Cross-browser XHR
-            // from https://gist.github.com/991713
-            getXMLObj = function (s, a) {
-                a = [a = "Msxml2.XMLHTTP", a + ".3.0", a + ".6.0"];
-                do
-                try {
-                    s = a.pop();
-                    return new(s ? ActiveXObject : XMLHttpRequest)(s)
-                } catch (e) {;
+			getUrl = function (url, callback) {
+				var xhr = new XMLHttpRequest();
+				xhr.open("GET", url, true);
+				xhr.onreadystatechange = function (e) {
+					if (xhr.readyState === 4) {
+						callback(xhr.responseText);
+					}
+				};
+				xhr.send();
+			},
 
-                }
-                while (s)
-            },
+			saveUrl = function(url, key, callback){
+				getUrl(url, function (text) {
+				   localStorage.setItem(key, text);
+				   
+				   if(isFunc(callback)){
+						callback();
+					}
+				});
+			},
 
-            getUrl = function (url, callback) {
-                var xhr = getXMLObj();
-                xhr.open("GET", url, true);
-                xhr.onreadystatechange = function (e) {
-                    if (xhr.readyState === 4) {
-                        callback(xhr.responseText);
-                    }
-                };
-                xhr.send();
-            },
+			isFunc =  function(func){
+				 return Object.prototype.toString.call(func) == "[object Function]";
+			},
 
-            saveUrl= function(url, key){
-                getUrl(url, function (text) {
-                   localStorage.setItem(key, text);
-                });
-            },
+			injectScript = function (text) {
+				var script = document.createElement("script"),
+					head = document.head || document.getElementsByTagName("head")[0];
 
-            injectScript = function (text) {
-                var script = d.createElement("script"),
-                    fragment = d.createDocumentFragment(),
-                    head = d.head || d.getElementsByTagName("head")[0];
-                script.defer = true;
-                script.appendChild(d.createTextNode(text));
-                fragment.appendChild(script);
-                head.appendChild(fragment);
-            },
+				script.defer = true;
+				script.appendChild(document.createTextNode(text));
+				head.appendChild(script);
+			},
 
-            queueExec = function (waitCount) {
-                var script, i, j, callback;
+			queueExec = function (waitCount) {
+				var script, i, j, callback;
 
-                if (scriptsExecuted >= waitCount) {
-                    for (i = 0; i < scripts.length; i++) {
-                        script = scripts[i];
+				if (scriptsExecuted >= waitCount) {
+					for (i = 0; i < scripts.length; i++) {
+						script = scripts[i];
 
-                        if (!script) {
-                            // loading/executed
-                            continue;
-                        }
-                        scripts[i] = null;
-                        injectScript(script);
-                        scriptsExecuted++;
+						if (!script) {
+							// loading/executed
+							continue;
+						}
+						scripts[i] = null;
+						injectScript(script);
+						scriptsExecuted++;
 
-                        for (j = i; j < scriptsExecuted; j++) {
-                            if (callback = waitCallbacks[j]) {
-                                waitCallbacks[j] = null;
-                                callback();
-                            }
-                        }
-                    }
-                }
-            };
+						for (j = i; j < scriptsExecuted; j++) {
+							if (callback = waitCallbacks[j]) {
+								waitCallbacks[j] = null;
+								callback();
+							}
+						}
+					}
+				}
+			};
 
-        return {
+		return {
 
-            require: function (uri) {
-                var key = _storagePrefix + uri,
-                    scriptIndex = scripts.length,
-                    _waitCount = waitCount;
+			require: function (uri) {
+				var key = _storagePrefix + uri,
+					scriptIndex = scripts.length,
+					_waitCount = waitCount,
+					source = localStorage.getItem(key);
 
-                scripts[scriptIndex] = null;
+				scripts[scriptIndex] = null;
 
-                if (_localStorage && localStorage.getItem(key)) {
-                    scripts[scriptIndex] = localStorage.getItem(key);
-                    queueExec(_waitCount);
-                } else {
-                    getUrl(uri, function (text) {
-                        (_localStorage) && localStorage.setItem(key, text);
-                        scripts[scriptIndex] = text;
-                        queueExec(_waitCount);
-                    });
-                }
-                return this;
-            },
+				if (source) {
+					scripts[scriptIndex] = source;
+					queueExec(_waitCount);
+				} else {
+					getUrl(uri, function (text) {
+						localStorage.setItem(key, text);
+						scripts[scriptIndex] = text;
+						queueExec(_waitCount);
+					});
+				}
+				return this;
+			},
 
-            add: function(uri, overwrite, callback){
-                var key = _storagePrefix + uri;
-               if(!!localStorage.getItem(key)){
-                   if(!!overwrite){
-                    saveUrl(uri, key);
-                   }
-               }else{
-                    saveUrl(uri, key);
-               }
-               if(callback){
-                   callback();
-               }
-               return this;
-            },
+			add: function(uri, overwrite, callback){
+				var key = _storagePrefix + uri;
 
-            remove: function(uri){
-                var key = _storagePrefix + uri;
-                localStorage.removeItem(key);
-                return this;
-            },
+				// default is to overwrite
+				if(overwrite === undefined){
+					overwrite = true;
+				}
 
-            wait: function (callback) {
-                waitCount = scripts.length;
-                if (callback) {
-                    (scriptsExecuted >= waitCount - 1) ? callback() : waitCallbacks[waitCount - 1] = callback;
-                }
+			   // if they key exists and overwrite true, overwrite
+			   if(!!localStorage.getItem(key)){
+					if(!!overwrite){
+						saveUrl(uri, key, callback);
+					}
+			   }else{
+					//key doesnt exist, add key as new entry
+					saveUrl(uri, key, callback);
+			   }
+			   return this;
+			},
 
-                return this;
-            }
-        };
+			remove: function(uri){
+				var key = _storagePrefix + uri;
+				localStorage.removeItem(key);
+				return this;
+			},
 
-    }
+			wait: function (callback) {
+				waitCount = scripts.length;
+				if (callback) {
+					(scriptsExecuted >= waitCount - 1) ? callback() : waitCallbacks[waitCount - 1] = callback;
+				}
+
+				return this;
+			}
+		};
+
+	}
 
 
-    w['basket'] = basketLoader();
+	window["basket"] = basketLoader();
 
 })(this, document);
